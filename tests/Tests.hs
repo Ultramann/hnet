a4770d46095263ca2e6dab855e4f6a6de9d11a5c
Ultramann <cary.goltermann@gmail.com>
2017-08-27 13:23:57 -0700

    Add backward pass tests

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -12,10 +12,12 @@ import Test.Hspec
    3                    ----     ----      ----
 -}
 
+zeroVec1 = vector [0]
 zeroVec2 = vector [0,0]
 zeroVec3 = vector [0,0,0]
 zeroD1 = Dense (matrix 3 (replicate 6 0)) zeroVec3
-zeroNet = Network [zeroD1, Dense (matrix 1 [0,0,0]) (vector [0])] [relu, relu]
+zeroNet = Network [zeroD1, Dense (matrix 1 [0,0,0]) zeroVec1] [relu, relu]
+nonZeroVec1 = vector [1]
 nonZeroVec2 = vector [-1.5,2]
 nonZeroVec3 = vector [1,2,3]
 simpleD1 = Dense (matrix 2 [3,-1,2,2,-2,3]) nonZeroVec2
@@ -25,9 +27,11 @@ simpleNet = Network [simpleD1, Dense w2 b2] [relu, relu]
 
 main :: IO ()
 main = hspec $ do
-  describe "Check dense:" $ do
+-------------------------------------------------------------------------------------------------
 
-    describe "feed forward:" $ do
+-------------------------------------------------------------------------------------------------
+  describe "Test feedForward:" $ do
+    describe "- dense:" $ do
       it "passes zero through zero case" $ do
         feedForward zeroD1 zeroVec2 `shouldBe` zeroVec3
       it "passes non-zero through zero case" $ do
@@ -36,20 +40,7 @@ main = hspec $ do
         feedForward simpleD1 zeroVec3 `shouldBe` nonZeroVec2
       it "passes simple through simple case" $ do
         feedForward simpleD1 nonZeroVec3 `shouldBe` vector [-0.5,14]
-
-    describe "pass backward:" $ do
-      it "passes zero through zero case" $ do
-        passBackward zeroD1 zeroVec3 `shouldBe` zeroVec2
-      it "passes non-zero through zero case" $ do
-        passBackward zeroD1 nonZeroVec3 `shouldBe` zeroVec2
-      it "passes zero through simple case" $ do
-        passBackward simpleD1 zeroVec2 `shouldBe` zeroVec3
-      it "passes simple through simple case" $ do
-        passBackward simpleD1 nonZeroVec2 `shouldBe` vector [-6.5,1,9]
-
-  describe "Check relu:" $ do
-
-    describe "feed forward:" $ do
+    describe "- relu:" $ do
       it "passes zero case" $ do
         feedForward relu zeroVec2 `shouldBe` zeroVec2
       it "passes positives case" $ do
@@ -58,8 +49,20 @@ main = hspec $ do
         feedForward relu (negate nonZeroVec3) `shouldBe` zeroVec3
       it "passes positives & negatives case" $ do
         feedForward relu nonZeroVec2 `shouldBe` vector [0,2]
+-------------------------------------------------------------------------------------------------
 
-    describe "pass backward:" $ do
+-------------------------------------------------------------------------------------------------
+  describe "Test passBackward:" $ do
+    describe "- dense:" $ do
+      it "passes zero through zero case" $ do
+        passBackward zeroD1 zeroVec3 `shouldBe` zeroVec2
+      it "passes non-zero through zero case" $ do
+        passBackward zeroD1 nonZeroVec3 `shouldBe` zeroVec2
+      it "passes zero through simple case" $ do
+        passBackward simpleD1 zeroVec2 `shouldBe` zeroVec3
+      it "passes non-zero through simple case" $ do
+        passBackward simpleD1 nonZeroVec2 `shouldBe` vector [-6.5,1,9]
+    describe "- relu:" $ do
       it "passes zero case" $ do
         passBackward relu zeroVec2 `shouldBe` zeroVec2
       it "passes positives case" $ do
@@ -68,33 +71,35 @@ main = hspec $ do
         passBackward relu (negate nonZeroVec3) `shouldBe` zeroVec3
       it "passes positives & negatives case" $ do
         passBackward relu nonZeroVec2 `shouldBe` vector [0,1]
-{-
-  describe "Check forward pass" $ do
-    it "passes trivial case" $ do
-      let zp = [(zeroVec, zeroVec), (zeroVec, zeroVec),
-                (vector [0.0], vector [0.0])]
-      (forwardPass zeroVec zeroNet) `shouldBe` zp
-    it "passes simple case" $ do
-      let fp = [(vector [1.0,2.0,3.0], vector [1.0,2.0,3.0]),
-                (vector [-0.5,14.0], vector [0.0,14.0]),
-                (vector [4.0], vector [4.0])]
-      (forwardPass vTest simpleNet) `shouldBe` fp
+-------------------------------------------------------------------------------------------------
 
-  describe "Check chain" $ do
-    it "passes trivial case" $ do
-      let (_, _, zm) = last zeroNet
-      let ze = vector [0.0]
-      (chain (zeroVec, zm) ze) `shouldBe` zeroVec
-    it "passes simple case" $ do
-      let tl = vector [-0.5,14]
-      let (_, _, lw) = last simpleNet
-      let e = vector [0.5]
-      let chained = vector [0,0.25]
-      (chain (tl, lw) e) `shouldBe` chained
+-------------------------------------------------------------------------------------------------
+  describe "Test forwardPass:" $ do
+    it "passes zero through zero case" $ do
+      forwardPass zeroVec2 zeroNet `shouldBe` ([zeroVec2, zeroVec3, zeroVec1],
+                                               [zeroVec2, zeroVec3, zeroVec1])
+    it "passes non-zero through zero case" $ do
+      forwardPass nonZeroVec2 zeroNet `shouldBe` ([nonZeroVec2, zeroVec3, zeroVec1],
+                                                  [nonZeroVec2, zeroVec3, zeroVec1])
+    it "passes zero through simple case" $ do
+      forwardPass zeroVec3 simpleNet `shouldBe` ([zeroVec3, nonZeroVec2, vector [-2]],
+                                                 [zeroVec3, vector [0,2], zeroVec1])
+    it "passes non-zero through simple case" $ do
+      forwardPass nonZeroVec3 simpleNet `shouldBe` ([nonZeroVec3, vector [-0.5,14], vector [4]],
+                                                    [nonZeroVec3, vector [0,14], vector [4]])
+-------------------------------------------------------------------------------------------------
 
-  describe "Check back prop" $ do
-    it "passes simple case" $ do
-      let tls = [zeroVec, vector [0]]
-      let lws = map (\(_, _, x) -> x) zeroNet
-      (backProp (vector [0]) tls lws) `shouldBe` tls
--}
+-------------------------------------------------------------------------------------------------
+  describe "Test backPropagate:" $ do
+    it "passes zero through zero case" $ do
+      backPropagate zeroVec1 zeroNet [zeroVec3, zeroVec1]
+                          `shouldBe` [zeroVec2, zeroVec3, zeroVec1]
+    it "passes non-zero through zero case" $ do
+      backPropagate nonZeroVec1 zeroNet [zeroVec3, zeroVec1]
+                          `shouldBe` [zeroVec2, zeroVec3, vector [1]]
+    it "passes zero through simple case" $ do
+      backPropagate zeroVec1 simpleNet [nonZeroVec2, vector [-2]]
+                          `shouldBe` [zeroVec3, zeroVec2, zeroVec1]
+    it "passes non-zero through simple case" $ do
+      backPropagate nonZeroVec1 simpleNet [vector [-0.5,14], vector [4]]
+                          `shouldBe` [vector [-0.5,1,1.5], vector [-2,0.5], nonZeroVec1]
